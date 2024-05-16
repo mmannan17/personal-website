8de834625b6bc5db9e28a86ba2128c1a4c9b74a4
Mustafa Mannan <mannan2@usf.edu>
2024-05-16 15:56:36 -0400

    Resume Updated and Experience Section Updated

--- src/components/Pages/experience.jsx
+++ src/components/Pages/experience.jsx
@@ -6,13 +6,13 @@ import Nsbephone from '../images/nsbephone.png';
 
 const experiences = [
   {
-    title: "Student Developer",
+    title: "Developer Intern",
     company: "Mowasalat Karwa",
     period: "May 2022 - August 2022",
     description: [
       "Contributed to enhancing the Karwa app's bus schedule and notification system, working under the guidance of senior developers.",
       "Assisted in refining the real-time notification features and user interface, improving the efficiency of the app and making it easier for users to receive timely bus schedule updates.",
-      "Developed the taxi booking component of the app, including integrating features for booking, tracking, and managing taxi rides.",
+      "Supported in the development of the taxi booking component, including integrating features for booking, tracking, and managing taxi rides.",
       "Involved in every cycle of the software development lifecycle (SDLC), from initial requirement gathering and analysis, through design, implementation, and testing, to deployment and maintenance.",
       "Gained comprehensive experience in developing robust and user-centric applications, ensuring high standards of quality and performance.",
       "Improved technical skills and understanding of the complete software development process and the importance of collaboration and continuous improvement in delivering successful software solutions."
